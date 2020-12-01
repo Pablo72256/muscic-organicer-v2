89888d6df28041f0c4c7a6c1aa1a6a5e7ecf7639
pablo <pablomrmr@gmail.com>
2020-12-01 09:50:25 +0100

    03 Implementando listMaching

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -1,96 +1,107 @@
-import java.util.ArrayList;
-
-/**
- * A class to hold details of audio files.
- * This version can play the files.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2011.07.31
- */
-public class MusicOrganizer
-{
-    // An ArrayList for storing the file names of music files.
-    private ArrayList<String> files;
-    // A player for the music files.
-    private MusicPlayer player;
+        import java.util.ArrayList;
         
-    /**
-     * Create a MusicOrganizer
-     */
-    public MusicOrganizer()
-    {
-        files = new ArrayList<String>();
-        player = new MusicPlayer();
-    }
-    
-    /**
-     * Add a file to the collection.
-     * @param filename The file to be added.
-     */
-    public void addFile(String filename)
-    {
-        files.add(filename);
-    }
-    
-    /**
-     * Return the number of files in the collection.
-     * @return The number of files in the collection.
-     */
-    public int getNumberOfFiles()
-    {
-        return files.size();
-    }
-    
-    /**
-     * List a file from the collection.
-     * @param index The index of the file to be listed.
-     */
-    public void listFile(int index)
-    {
-        if(index >= 0 && index < files.size()) {
-            String filename = files.get(index);
-            System.out.println(filename);
-        }
-    }
-    
-    /**
-     * Remove a file from the collection.
-     * @param index The index of the file to be removed.
-     */
-    public void removeFile(int index)
-    {
-        if(index >= 0 && index < files.size()) {
-            files.remove(index);
-        }
-    }
-
-    /**
-     * Start playing a file in the collection.
-     * Use stopPlaying() to stop it playing.
-     * @param index The index of the file to be played.
-     */
-    public void startPlaying(int index)
-    {
-        String filename = files.get(index);
-        player.startPlaying(filename);
-    }
-
-    /**
-     * Stop the player.
-     */
-    public void stopPlaying()
-    {
-        player.stop();
-    }
-    
-    /**
-     * Listar todos los elementos de la coleccion.
-     */
-    public void listAllFiles(){
-        int contador = 1;
-        for (String filename : files) {
-            System.out.println(contador +"." + filename);
-            contador += 1;
-        }
-    }
+        /**
+         * A class to hold details of audio files.
+         * This version can play the files.
+         * 
+         * @author David J. Barnes and Michael Kölling
+         * @version 2011.07.31
+         */
+        public class MusicOrganizer
+        {
+            // An ArrayList for storing the file names of music files.
+            private ArrayList<String> files;
+            // A player for the music files.
+            private MusicPlayer player;
+                
+            /**
+             * Create a MusicOrganizer
+             */
+            public MusicOrganizer()
+            {
+                files = new ArrayList<String>();
+                player = new MusicPlayer();
+            }
+            
+            /**
+             * Add a file to the collection.
+             * @param filename The file to be added.
+             */
+            public void addFile(String filename)
+            {
+                files.add(filename);
+            }
+            
+            /**
+             * Return the number of files in the collection.
+             * @return The number of files in the collection.
+             */
+            public int getNumberOfFiles()
+            {
+                return files.size();
+            }
+            
+            /**
+             * List a file from the collection.
+             * @param index The index of the file to be listed.
+             */
+            public void listFile(int index)
+            {
+                if(index >= 0 && index < files.size()) {
+                    String filename = files.get(index);
+                    System.out.println(filename);
+                }
+            }
+            
+            /**
+             * Remove a file from the collection.
+             * @param index The index of the file to be removed.
+             */
+            public void removeFile(int index)
+            {
+                if(index >= 0 && index < files.size()) {
+                    files.remove(index);
+                }
+            }
+        
+            /**
+             * Start playing a file in the collection.
+             * Use stopPlaying() to stop it playing.
+             * @param index The index of the file to be played.
+             */
+            public void startPlaying(int index)
+            {
+                String filename = files.get(index);
+                player.startPlaying(filename);
+            }
+        
+            /**
+             * Stop the player.
+             */
+            public void stopPlaying()
+            {
+                player.stop();
+            }
+            
+            /**
+             * Listar todos los elementos de la coleccion.
+            */
+            public void listAllFiles(){
+                int contador = 1;
+                for (String filename : files) {
+                    System.out.println(contador +"." + filename);
+                    contador += 1;
+                }
+            }
+            
+            /**
+             * Listar todos los elementos con un String pasado como parametro
+            */
+            public void listMaching(String searchString){
+                for (String filename : files) {
+                    if (filename.contains(searchString)){
+                        System.out.println(filename);
+                    }
+                }
+            }
 }
